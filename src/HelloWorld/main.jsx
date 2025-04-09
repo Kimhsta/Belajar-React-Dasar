173d1e0b1cffce535fab1a3ae2ce913cd437495c
Kiki Mahesta <kimsta.ti@gmail.com>
2025-04-09 13:17:12 +0700

    add HTML entry point and main React rendering logic

---
+++ src/HelloWorld/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import HelloWorld from "./hello-world";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HelloWorld />
+  </StrictMode>
+);
