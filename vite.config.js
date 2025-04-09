6c31d02930a89850a91002ed4d0262a5920c1b30
Kiki Mahesta <kimsta.ti@gmail.com>
2025-04-09 13:25:36 +0700

    agar bisa berjalan multi page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import HelloWorld from './src/HelloWorld/hello-world'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for the build
+    rollupOptions: {
+      input: {
+        //Penadahan index
+        index: "index.html",
+        HelloWorld: "hello-world.html",
+      }
+    }
+  },
 })
