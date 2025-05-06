9c223b49083dd390ade0cede869b4db16c52a2e4
Kiki Mahesta <kimsta.ti@gmail.com>
2025-05-07 00:04:02 +0700

    update

--- src/HelloWorld/hello-world.jsx
+++ src/HelloWorld/hello-world.jsx
@@ -1,6 +1,7 @@
 export default function HelloWorld() {
   return (
     <div>
+      <img src={"../assets/chinese high school uniform girl student.jpeg"}/>
       <h1>Hello World!</h1>
       <p>This is a simple React component.</p>
     </div>
